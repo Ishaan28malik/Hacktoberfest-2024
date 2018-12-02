dc4ed947a682374e087eb99cee006ebb6e24f449
PythoShark(#techno utopian) <ishaanmalikrm7@gmail.com>
2018-12-02 15:29:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var button=document.querySelector("button");
 var filled=false;
-button.addEventListener("click",function(){ 
+button.addEventListener("click",function()){ 
 	//alert('connected');
 	if(filled){
 		document.body.style.background="blue";
@@ -11,4 +11,4 @@ button.addEventListener("click",function(){
 	else (){
 		document.body.style.background="silver";
 		filled=!filled;
-	});
+	};
