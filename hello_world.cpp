8bf84b90efb595efd69880c3ba96a69264ba1f79
s1042992 <40089665+s1042992@users.noreply.github.com>
2019-10-06 08:10:20 +0800

    Create hello world cpp file
    Create hello world cpp file...

---
+++ hello_world.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"GoodBye world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"GoodBye world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"GoodBye world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<"GoodBye world!"<<endl;
+}
