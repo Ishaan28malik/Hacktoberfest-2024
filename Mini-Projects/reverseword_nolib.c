50d84568f7651bbc3f4408905f54d0c5b6cd9fd5
DarthMuzammil <muzammil.inamdar98@gmail.com>
2020-09-26 01:53:43 +0530

    Added documentation for mini project

--- Mini-Projects/reverseword_nolib.c
+++ Mini-Projects/reverseword_nolib.c
@@ -3,6 +3,7 @@ Program to reverse each word in a string without any library functions
 Author - Muzammil | DarthMuzammil @Github
 */
 #include<stdio.h>
+//Function to swap two characters
 char* swap(char* str, char* str2){
     *str = *str + *str2;
     *str2 = *str - *str2;
@@ -10,6 +11,7 @@ char* swap(char* str, char* str2){
     return(NULL);
 }
 
+//Function to reverse a string from start position to end position
 char* reverse(char* str, int start, int end){
     while(start < end){
         swap(&str[start],&str[end]);
@@ -19,12 +21,13 @@ char* reverse(char* str, int start, int end){
     return(NULL);
 }
 
+//Function that takes string as input and reverses each word
 char* parse(char* str){
     int start, end;
     int i = 0; 
     int j =0;
     for( i = 0; ; i++){
-        if(str[i] == ' '){
+        if(str[i] == ' '){    //stop when whitespace is encountered 
             reverse(str, j, (i - 1));
             j = i + 1;
         }
