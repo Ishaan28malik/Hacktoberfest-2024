1f5b31bacc1b22dc90a236d5a4549f98e83b1982
Arief <mgi.official1@gmail.com>
2020-10-04 04:47:52 +0700

    fix some css file location

--- Mini-Projects/PHP/php/home.php
+++ Mini-Projects/PHP/php/home.php
@@ -7,11 +7,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <style>
-<?php include 'css/home.css'; ?>
+/* <?php include 'css/home.css'; ?> */
 </style>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="C:\xampp\htdocs\trekbuddy\css\home.css" type="text/css">
+    <!-- <link rel="stylesheet" href="C:\xampp\htdocs\trekbuddy\css\home.css" type="text/css"> -->
+    <link rel="stylesheet" href="../css/home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Home</title>
   </head>
--- Mini-Projects/PHP/php/jaisalmer.php
+++ Mini-Projects/PHP/php/jaisalmer.php
@@ -7,10 +7,8 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="C:\Users\omkar\Desktop\trekbuddy\jaisalmercss.css">
-    <style>
-<?php include 'css/jaisalmercss.css'; ?>
-</style>
+    <link rel="stylesheet" href="../css/jaisalmercss.css">
+
     <title>Jaisalmer Desert Camping</title>
   </head>
 
