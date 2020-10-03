0cd154a92b3f6c31a256e3f5e4259ecab56a614a
Harshit Gupta <hgupta07@in.ibm.com>
2020-10-03 11:38:30 +0530

    Changes regarding exported funcs and underscore

--- Mini-Projects/GO/FindARestaurant/findARestaurant.go
+++ Mini-Projects/GO/FindARestaurant/findARestaurant.go
@@ -8,16 +8,16 @@ import (
 	"strings"
 )
 
-var foursquare_client_id = "PQSPJYQ4ODOORBDB51EQTURIFOQT0PACPFQI2UN0G0P00DAF"
-var foursquare_client_secret = "3ZUE0PGPDY2KV4UFPWEQGLZ4GNDWC2PZHFTX40CKZTCIA3LP"
+var foursquareClientId = ""
+var foursquareClientSecret = ""
 
-func FindARestaurant(mealType string, location string) {
+func findARestaurant(mealType string, location string) {
 
-	latitude, longitude := GetGeocodeLocation(location)
+	latitude, longitude := getGeocodeLocation(location)
 	lat := fmt.Sprintf("%f", latitude)
 	long := fmt.Sprintf("%f", longitude)
 
-	url := "https://api.foursquare.com/v2/venues/search?client_id=" + foursquare_client_id + "&client_secret=" + foursquare_client_secret + "&v=20130815&ll=" + lat + "," + long + "&query=" + mealType
+	url := "https://api.foursquare.com/v2/venues/search?client_id=" + foursquareClientId + "&client_secret=" + foursquareClientSecret + "&v=20130815&ll=" + lat + "," + long + "&query=" + mealType
 
 	//fmt.Println(url)
 	resp, err := http.Get(url)
@@ -34,9 +34,9 @@ func FindARestaurant(mealType string, location string) {
 
 		for _, v2 := range v.([]interface{}) {
 			id := v2.(map[string]interface{})["id"].(string)
-			rest_name := v2.(map[string]interface{})["name"]
-			rest_address := v2.(map[string]interface{})["location"].(map[string]interface{})["formattedAddress"]
-			Imgurl := "https://api.foursquare.com/v2/venues/" + id + "/photos?client_id=" + foursquare_client_id + "&v=20150603&client_secret=" + foursquare_client_secret
+			restName := v2.(map[string]interface{})["name"]
+			restAddress := v2.(map[string]interface{})["location"].(map[string]interface{})["formattedAddress"]
+			Imgurl := "https://api.foursquare.com/v2/venues/" + id + "/photos?client_id=" + foursquareClientId + "&v=20150603&client_secret=" + foursquare_client_secret
 			imageResp, err3 := http.Get(Imgurl)
 			RespBody, err4 := ioutil.ReadAll(imageResp.Body)
 			if err3 != nil || err4 != nil {
@@ -68,8 +68,8 @@ func FindARestaurant(mealType string, location string) {
 			}
 
 			restaurantInfo := map[string]interface{}{
-				"name":    rest_name,
-				"address": rest_address,
+				"name":    restName,
+				"address": restAddress,
 				"image":   imageURL}
 
 			fmt.Println(restaurantInfo)
@@ -80,8 +80,8 @@ func FindARestaurant(mealType string, location string) {
 	}
 }
 
-func GetGeocodeLocation(s string) (float64, float64) {
-	google_api_key := "AIzaSyAcKRO63SCy87HvebSXO0v6Gjp444PwNG8"
+func getGeocodeLocation(s string) (float64, float64) {
+	google_api_key := ""
 	locationString := strings.ReplaceAll(s, " ", "+")
 	url := ("https://maps.googleapis.com/maps/api/geocode/json?address=" + locationString + "&key=" + google_api_key)
 	response, err := http.Get(url)
@@ -112,5 +112,5 @@ func GetGeocodeLocation(s string) (float64, float64) {
 }
 
 func main() {
-	FindARestaurant("Pizza", "Pune, India")
+	findARestaurant("Pizza", "Pune, India")
 }
