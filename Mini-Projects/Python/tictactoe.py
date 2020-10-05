71f149f8cd2b5806502edf60b91fd9ce9db33da5
Raj Banerjee <44421352+Raj-Banerjee@users.noreply.github.com>
2020-10-05 17:39:30 +0530

    Fixing the errors in the Code

--- Mini-Projects/Python/tictactoe.py
+++ Mini-Projects/Python/tictactoe.py
@@ -1,30 +1,30 @@
 def print_board(board):
 
-	print "The board look like this: \n"
+	print ("The board look like this: \n")
 
 	for i in range(3):
-		print " ",
+		print (" "),
 		for j in range(3):
 			if board[i*3+j] == 1:
-				print 'X',
+				print ('X'),
 			elif board[i*3+j] == 0:
-				print 'O',	
+				print ('O'),	
 			elif board[i*3+j] != -1:
-				print board[i*3+j]-1,
+				print (board[i*3+j]-1),
 			else:
-				print ' ',
+				print (' '),
 			
 			if j != 2:
-				print " | ",
+				print (" | "),
 		print
 		
 		if i != 2:
-			print "-----------------"
+			print ("-----------------")
 		else: 
 			print 
 			
 def print_instruction():
-	print "Please use the following cell numbers to make your move"
+	print ("Please use the following cell numbers to make your move")
 	print_board([2,3,4,5,6,7,8,9,10])
 
 
@@ -33,15 +33,15 @@ def get_input(turn):
 	valid = False
 	while not valid:
 		try:
-			user = raw_input("Where would you like to place " + turn + " (1-9)? ")
+			user = input("Where would you like to place " + turn + " (1-9)? ")
 			user = int(user)
 			if user >= 1 and user <= 9:
 				return user-1
 			else:
-				print "That is not a valid move! Please try again.\n"
+				print ("That is not a valid move! Please try again.\n")
 				print_instruction()
 		except Exception as e:
-			print user + " is not a valid move! Please try again.\n"
+			print (user + " is not a valid move! Please try again.\n")
 		
 def check_win(board):
 	win_cond = ((1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(3,5,7))
@@ -55,7 +55,7 @@ def check_win(board):
 
 def quit_game(board,msg):
 	print_board(board)
-	print msg
+	print (msg)
 	quit()
 
 def main():
@@ -77,7 +77,7 @@ def main():
 
 		# print board
 		print_board(board)
-		print "Turn number " + str(move+1)
+		print ("Turn number " + str(move+1))
 		if move % 2 == 0:
 			turn = 'X'
 		else:
@@ -86,7 +86,7 @@ def main():
 		# get user input
 		user = get_input(turn)
 		while board[user] != -1:
-			print "Invalid move! Cell already taken. Please try again.\n"
+			print ("Invalid move! Cell already taken. Please try again.\n")
 			user = get_input(turn)
 		board[user] = 1 if turn == 'X' else 0
 
