736929b2f0ddf701564519d45a9fbbbae7e12f36
Musavveer Rehaman <iammusavveer@gmail.com>
2020-10-05 10:08:38 +0530

    Added Python practice problems

---
+++ Mini-Projects/Python/greatest_variable.py
@@ -0,0 +1,12 @@
+i=input('Enter the value of i:')
+j=input('Enter the value of j:')
+k=input('Enter the value of k:')
+
+if i>j and i>k:
+    print("i is Greatest")
+
+elif j>i and j>k: 
+    print("j is Greatest")   
+
+else:
+    print("k is Greatest")
