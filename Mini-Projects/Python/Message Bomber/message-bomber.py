90cdbbca55cb32646af6507c1454f7ba189366e7
Amankazz <40912399+Amankazz@users.noreply.github.com>
2020-10-05 17:46:11 +0530

    Updated Message Bomber
    This python program is used to send desired number of amazon O.T.P. messages to the victim whose number is entered.

--- Mini-Projects/Python/Message Bomber/message-bomber.py
+++ Mini-Projects/Python/Message Bomber/message-bomber.py
@@ -7,7 +7,7 @@ amt = int(input("How many times you want to send OTP:"))
 # 1> download google chrome webdriver
 
 
-browser = webdriver.Chrome('D:\\Super Brain\\Automation with python\\Chrome Driver\\chromedriver.exe')
+browser = webdriver.Chrome('---location of your google chrome driver---')
 
 browser.get('https://www.amazon.in/ap/signin?_encoding=UTF8&ignoreAuthState=1&openid.assoc_handle=inflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_custrec_signin&switch_account=')
 
