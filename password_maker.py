c513480d1f8fa6982e660f24c2a021cb9436917e
Priyansh Kulshrestha <65914664+priyanshkulshrestha@users.noreply.github.com>
2020-10-07 19:12:46 +0530

    Random-Password-maker.py
    this will create a random password of the given length which is to hard to crack.

---
+++ password_maker.py
@@ -0,0 +1,22 @@
+import string
+import random
+if __name__ == "__main__":
+    s1 = string.ascii_lowercase
+    # print(s1)
+    s2 = string.ascii_uppercase
+    # print(s2)
+    s3 = string.digits
+    # print(s3)
+    s4 = string.punctuation
+    # print(s4)
+    plen = int(input("enter password len"))
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    # print(s)
+    random.shuffle(s)
+    # print(s)
+    print("".join(s[0:plen]))
+    # saving password in a files
