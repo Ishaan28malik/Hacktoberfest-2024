d832893cae1a20cad266ae68c60dbc018fb78c04
ManjushaTVS <69451319+ManjushaTVS@users.noreply.github.com>
2020-10-14 20:12:56 +0530

    updated factorial.py
    updated factorial algo

--- Mini-Projects/Python/factorial.py
+++ Mini-Projects/Python/factorial.py
@@ -1,16 +1,5 @@
-num = int(input('Enter the number: '))
-
-factorial = 1
-
-if num<0:
-  print("Factorial doesn't exist")  
-
-elif num==0:
-  print("Factorial is 1")
-
-else:
-  for i in range(1,num+1):
-    factorial = factorial*i
-    
-
-  print("Factorial is",factorial)    
+num=int(input("enter number"))
+fact = 1
+for i in range(1,n+1):
+  fact = fact*i
+print(fact)
