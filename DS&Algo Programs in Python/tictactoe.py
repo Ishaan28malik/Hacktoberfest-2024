4333418ca3cb4e17ba3bbab821d7186ed47394a3
Jatin Saini <itsjatin135@gmail.com>
2020-10-15 10:06:15 +0530

    Update tictactoe.py

--- DS&Algo Programs in Python/tictactoe.py
+++ DS&Algo Programs in Python/tictactoe.py
@@ -1,103 +1,133 @@
-sample = [[1,2,3],[4,5,6],[7,8,9]]
+board = [' ' for x in range(10)]
 
-ar = [['','',''],
-      ['','',''],
-      ['','','']]
+def insertLetter(letter,pos):
+    board[pos] = letter
 
-def tictactoe():
-    print('Welcome to the TicTacToe game!')
-    print('In order to make your moves, please follow the positional map below:')
-    print_game(sample)
-    player = 'x'
-    while(1):
-        print(player, "'s turn")
-        user_input = input('Provide move:')
-        move = int(user_input)
-        playMove(move, player)
-        print_game(ar)
+def spaceIsFree(pos):
+    return board[pos] == ' '
 
-        if win(ar) == True:
-            print(player, ' wins the game!')
-            break
-        if draw(ar) == True:
-            print('Game ended in a draw!')
-            break
-            
-        if player == 'x':
-            player = 'o'
+def printBoard(board):
+    print('   |   |   ')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |   ')
+    print('------------')
+    print('   |   |   ')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |   ')
+    print('------------')
+    print('   |   |   ')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |   ')
+
+def isBoardFull(board):
+    if board.count(' ') > 1:
+        return False
+    else:
+        return True
+
+def IsWinner(b,l):
+    return ((b[1] == l and b[2] == l and b[3] == l) or
+    (b[4] == l and b[5] == l and b[6] == l) or
+    (b[7] == l and b[8] == l and b[9] == l) or
+    (b[1] == l and b[4] == l and b[7] == l) or
+    (b[2] == l and b[5] == l and b[8] == l) or
+    (b[3] == l and b[6] == l and b[9] == l) or
+    (b[1] == l and b[5] == l and b[9] == l) or
+    (b[3] == l and b[5] == l and b[7] == l))
+
+def playerMove():
+    run = True
+    while run:
+        move = input("please select a position to enter the X between 1 to 9")
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X' , move)
+                else:
+                    print('Sorry, this space is occupied')
+            else:
+                print('please type a number between 1 and 9')
+
+        except:
+            print('Please type a number')
+
+def computerMove():
+    possibleMoves = [x for x , letter in enumerate(board) if letter == ' ' and x != 0  ]
+    move = 0
+
+    for let in ['O' , 'X']:
+        for i in possibleMoves:
+            boardcopy = board[:]
+            boardcopy[i] = let
+            if IsWinner(boardcopy, let):
+                move = i
+                return move
+
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1 , 3 , 7 , 9]:
+            cornersOpen.append(i)
+
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2,4,6,8]:
+            edgesOpen.append(i)
+
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+        return move
+
+def selectRandom(li):
+    import random
+    ln = len(li)
+    r = random.randrange(0,ln)
+    return li[r]
+
+def main():
+    print("Welcome to the game!")
+    printBoard(board)
+
+    while not(isBoardFull(board)):
+        if not(IsWinner(board , 'O')):
+            playerMove()
+            printBoard(board)
         else:
-            player = 'x'
+            print("sorry you loose!")
+            break
 
-def playMove(move, player):
-    if move > 0 and move <= 9:
-            if move == 1:
-                if ar[0][0] == '':
-                    ar[0][0] = player
-            elif move == 2:
-                if ar[0][1] == '':
-                    ar[0][1] = player
-            elif move == 3:
-                if ar[0][2] == '':
-                    ar[0][2] = player
-            elif move == 4:
-                if ar[1][0] == '':
-                    ar[1][0] = player
-            elif move == 5:
-                if ar[1][1] == '':
-                    ar[1][1] = player
-            elif move == 6:
-                if ar[1][2] == '':
-                    ar[1][2] = player
-            elif move == 7:
-                if ar[2][0] == '':
-                    ar[2][0] = player
-            elif move == 8:
-                if ar[2][1] == '':
-                    ar[2][1] = player
-            elif move == 9:
-                if ar[2][2] == '':
-                    ar[2][2] = player
+        if not(IsWinner(board , 'X')):
+            move = computerMove()
+            if move == 0:
+                print(" ")
+            else:
+                insertLetter('O' , move)
+                print('computer placed an o on position' , move , ':')
+                printBoard(board)
+        else:
+            print("you win!")
+            break
 
-def win(ar):
-    if ar[0][0] == ar[0][1] == ar[0][2] == 'x' \
-       or ar[1][0] == ar[1][1] == ar[1][2] == 'x' \
-       or ar[2][0] == ar[2][1] == ar[2][2] == 'x':
-        return True
 
-    if ar[0][0] == ar[1][0] == ar[2][0] == 'x' \
-       or ar[0][1] == ar[1][1] == ar[2][1] == 'x' \
-       or ar[0][2] == ar[1][2] == ar[2][2] == 'x':
-        return True
 
-    if ar[0][0] == ar[1][1] == ar[2][2] == 'x' \
-       or ar[2][0] == ar[1][1] == ar[0][2] == 'x':
-        return True
-    
-    if ar[0][0] == ar[0][1] == ar[0][2] == 'o' \
-       or ar[1][0] == ar[1][1] == ar[1][2] == 'o' \
-       or ar[2][0] == ar[2][1] == ar[2][2] == 'o':
-        return True
 
-    if ar[0][0] == ar[1][0] == ar[2][0] == 'o' \
-       or ar[0][1] == ar[1][1] == ar[2][1] == 'o' \
-       or ar[0][2] == ar[1][2] == ar[2][2] == 'o':
-        return True
+    if isBoardFull(board):
+        print("Tie game")
 
-    if ar[0][0] == ar[1][1] == ar[2][2] == 'o' \
-       or ar[2][0] == ar[1][1] == ar[0][2] == 'o':
-        return True
-
-    return False
-
-def draw(ar):
-    for i in ar:
-        for j in i:
-            if j == '':
-                return False
-    return True
-
-def print_game(ar):
-    for i in ar:
-        print(i)
-        
-tictactoe()
+while True:
+    x = input("Do you want to play again? (y/n)")
+    if x.lower() == 'y':
+        board = [' ' for x in range(10)]
+        print('--------------------')
+        main()
+    else:
+        break
