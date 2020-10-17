ea3efcff8e81b5dd242e24c08751428e1ba1a941
Gaurav Kumar <gauravkumar.bokaro@gmail.com>
2020-10-17 15:48:23 +0530

    Create SwapNumber.java

---
+++ SwapNumber.java
@@ -0,0 +1,17 @@
+import java.util.*;  
+class Swap_With {  
+    public static void main(String[] args) {  
+       int x, y, t;// x and y are to swap   
+       Scanner sc = new Scanner(System.in);  
+       System.out.println("Enter the value of X and Y");  
+       x = sc.nextInt();  
+       y = sc.nextInt();  
+       System.out.println("before swapping numbers: "+x +"  "+ y);  
+       /*swapping */  
+       t = x;  
+       x = y;  
+       y = t;  
+       System.out.println("After swapping: "+x +"   " + y);  
+       System.out.println( );  
+    }    
+}  
