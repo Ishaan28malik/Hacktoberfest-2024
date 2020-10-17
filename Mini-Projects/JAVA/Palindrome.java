c0b99c0d6e6de121ac0b112b4516e16118f1d276
Krishanu Chakraborty <shanu.16k@gmail.com>
2020-10-17 22:12:21 +0530

    Integer Palindrome

---
+++ Mini-Projects/JAVA/Palindrome.java
@@ -0,0 +1,44 @@
+package testing;
+import java.util.Scanner;
+/**
+ * Java program to check if the number is palindrome or not.
+ * A number is called palindrome if the number and its reverse is equal
+ * This Java program can also be used to reverse a number in Java
+ */
+public class NoClassDefFoundErrorDueToStaticInitFailure {
+
+    public static void main(String args[]){
+       
+        System.out.println("Please Enter a number : ");
+        int palindrome = new Scanner(System.in).nextInt();
+       
+        if(isPalindrome(palindrome)){
+            System.out.println("Number : " + palindrome + " is a palindrome");
+        }else{
+            System.out.println("Number : " + palindrome + " is not a palindrome");
+        }      
+       
+    }
+ 
+    /*
+     * Java method to check if a number is palindrome or not
+     */
+    public static boolean isPalindrome(int number) {
+        int palindrome = number; // copied number into variable
+        int reverse = 0;
+
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+
+        // if original and reverse of number is equal means
+        // number is palindrome in Java
+        if (number == reverse) {
+            return true;
+        }
+        return false;
+    }
+
+}
