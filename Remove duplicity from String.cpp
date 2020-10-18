bead731a5a0e1590d3fb0022a95f39dc6ac9657f
Gurpreet Singh Sahni <47444537+gurpreetsahni@users.noreply.github.com>
2020-10-18 13:16:11 +0530

    Add files via upload

---
+++ Remove duplicity from String.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string sent,word;
+    vector <string> hsh;
+    getline(cin,sent);
+    stringstream sh(sent);
+    do
+    {
+        sh>>word;
+        auto ser=find(hsh.begin(),hsh.end(),word);
+        if(ser==hsh.end())
+        {
+            cout<<word<<" ";
+            hsh.push_back(word);
+        }
+
+    }while(sh);
+    return 0;
+}
