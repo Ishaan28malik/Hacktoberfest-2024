7ffe4b299a15c0c59b8f73d5b5a1ce03e3fa8946
Rajat dua <48120175+rajatdua77@users.noreply.github.com>
2020-10-18 19:30:23 +0530

    Create reverse-string.java
    rajatdua77

---
+++ reverse-string.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class DP_Reverse
+{
+  public static void main(String args[])
+  {
+    String original, reverse = "";
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Enter a string to reverse");
+    original = in.nextLine();
+
+    int length = original.length();
+
+    for (int i = length - 1 ; i >= 0 ; i--)
+      reverse = reverse + original.charAt(i);
+
+    System.out.println("Reverse of the string: " + reverse);
+  }
+}
