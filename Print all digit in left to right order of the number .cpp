a546432125adaea660baab8d7c3e797c2d205243
Gurpreet Singh Sahni <47444537+gurpreetsahni@users.noreply.github.com>
2020-10-18 13:41:08 +0530

    Add files via upload

---
+++ Print all digit in left to right order of the number .cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+void digit(long n)
+{
+    int r;
+    if(n==0)
+        return ;
+    r=n%10;
+    digit(n/10);
+    cout<<r<<endl;
+
+}
+
+
+int main()
+{
+    long n;
+    cin>>n;
+    digit(n);
+    return 0;
+}
