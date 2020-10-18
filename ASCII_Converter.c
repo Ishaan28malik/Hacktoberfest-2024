c08273d166163244db2f53dee1aa44a3b34b1bd1
Aditya Patil <patiladitya1495@gmail.com>
2020-10-18 15:00:17 +0530

    Create ASCII_Converter.c

---
+++ ASCII_Converter.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter any character:");
+    scanf("%c", &ch);
+    printf("ASCII value of character %c is: %d", ch, ch);
+    return 0;
+}
