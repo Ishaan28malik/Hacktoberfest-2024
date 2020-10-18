6753f459407f0bb5a93834aba1889f33cd127f07
Abhijeet Bhattacharya <31439714+Abhijeet399@users.noreply.github.com>
2020-10-18 20:03:11 +0530

    Add files via upload

---
+++ calci_arduino/calci_arduino.ino
@@ -0,0 +1,26 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+int a,b;
+}
+
+void loop() {
+  int a,b,j;
+  // put your main code here, to run repeatedly:
+Serial.println("ao dekho");
+while(!Serial.available()){
+  
+}
+a=Serial.read();
+while(!Serial.available()){
+}
+Serial.println(a);
+
+b=Serial.read();
+while(!Serial.available()){
+}
+Serial.println(b);
+
+j=a+b;
+Serial.println(j);
+}
