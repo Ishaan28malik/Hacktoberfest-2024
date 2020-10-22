b93d19bb9fac0bedaaf5d2c4451720a44c670f95
stee1ix <rituraj.ranjan.42@gmail.com>
2020-10-22 11:19:32 +0530

    added a pdf merging python script

---
+++ Mini-Projects/Python/merging_two_pdfs.py
@@ -0,0 +1,15 @@
+#FORMAT ->  python3 merging_two_pdfs.py filename1 filename2
+
+import PyPDF2
+import sys
+
+inputs = sys.argv[1:]
+
+def pdf_combiner(pdf_list):
+	merger = PyPDF2.PdfFileMerger()
+	for pdf in pdf_list:
+		print(pdf)
+		merger.append(pdf)
+	merger.write('merged.pdf')
+
+pdf_combiner(inputs)
