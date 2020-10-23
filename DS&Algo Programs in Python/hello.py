74f51ea1e5e6bfd897eda3cd4f5cd097ba23e126
flappybird-code <73328484+flappybird-code@users.noreply.github.com>
2020-10-23 12:27:23 +0530

    Updated

--- DS&Algo Programs in Python/hello.py
+++ DS&Algo Programs in Python/hello.py
@@ -1,12 +1,5 @@
-
-
-print('Hello World!')
-
-#fazendo um print com formatação, variáveis e função.
-
-def main():
-  mensagem = input('Digite a mensagem de apresentação : ')
-  
-  print(f' A mensagem digitada foi : {mensagem} !')
-main()  
+#this
+#is a
+#comment
+print("hello world")
 
