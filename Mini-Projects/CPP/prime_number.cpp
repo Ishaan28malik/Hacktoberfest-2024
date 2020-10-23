80b068b306408abd41ac64fe8af9c0376c864e95
flappybird-code <73328484+flappybird-code@users.noreply.github.com>
2020-10-23 12:32:52 +0530

    Update prime_number.cpp

--- Mini-Projects/CPP/prime_number.cpp
+++ Mini-Projects/CPP/prime_number.cpp
@@ -1,29 +1,16 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int divisor, number_entered;
-    bool isPrime = true;
-
-    cout << "Enter a positive integer: ";
-    cin >> number_entered;
-
-    // 0 and 1 are not prime numbers
-    if (number_entered == 0 || number_entered == 1) {
-        isPrime = false;
-    }
-    else {
-        for (divisor = 2; divisor <= number_entered / 2; ++divisor) {
-            if (number_entered % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-    }
-    if (isPrime)
-        cout << number_entered << " is a prime number";
-    else
-        cout << number_entered << " is not a prime number";
-
-    return 0;
-}
+# Function to print binary number for the
+# input decimal using recursion
+def decimalToBinary(n):
+if(n > 1):
+# divide with integral result
+# (discard remainder)
+decimalToBinary(n//2)
+print(n%2, end=' ')
+# Driver code
+if __name__ == '__main__':
+decimalToBinary(8)
+print("n")
+decimalToBinary(18)
+print("n")
+decimalToBinary(7)
+print("n") 
