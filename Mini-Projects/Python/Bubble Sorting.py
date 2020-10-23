ca3bfbc0c059a74afb4dc3afde694edef3325a8e
flappybird-code <73328484+flappybird-code@users.noreply.github.com>
2020-10-23 12:35:47 +0530

    Update Bubble Sorting.py

--- Mini-Projects/Python/Bubble Sorting.py
+++ Mini-Projects/Python/Bubble Sorting.py
@@ -1,29 +1,26 @@
-'''
-Implementation of Binary Search Algorithm in Python
-'''
-
-# defining a function for bubble sort
-def bubble_sort(my_list):
-
-                # len() to find total no of elements in list
-                length   =  len(my_list)
-                print(f"Original List : {my_list}")
-
-                # traversing through the list with for loop
-                for i in range(length - 1): 
-                        for j in range(length - i - 1):
-                                #  checking and swapping values if the element is found larger
-                                if my_list[j] > my_list[j+1]:
-                                        my_list[j],  my_list[j+1] = my_list[j+1],  my_list[j]
-
-                print(f"After sorting list is :  {my_list}")
-
-# creating a list through user input
-my_list = []
-num = int(input("Enter total no of elements =  "))
-for i in range(num):
-        x = int(input("Enter no :  "))
-        my_list.append(x)
-
-# function call
-bubble_sort(my_list)
+# Python program for implementation of Bubble Sort 
+  
+def bubbleSort(arr): 
+    n = len(arr) 
+  
+    # Traverse through all array elements 
+    for i in range(n-1): 
+    # range(n) also work but outer loop will repeat one time more than needed. 
+  
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+  
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+  
+# Driver code to test above 
+arr = [64, 34, 25, 12, 22, 11, 90] 
+  
+bubbleSort(arr) 
+  
+print ("Sorted array is:") 
+for i in range(len(arr)): 
+    print ("%d" %arr[i]),  
