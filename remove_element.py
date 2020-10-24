cd0f7c69bc5bdc8715c0ba0a3ec2df3890891107
kanika1006 <33006678+kanika1006@users.noreply.github.com>
2020-10-24 12:42:06 +0530

    Create remove_element.py

---
+++ remove_element.py
@@ -0,0 +1,24 @@
+/*
+
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+*/
+
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:        
+        while val in nums:
+            del nums[nums.index(val)]
