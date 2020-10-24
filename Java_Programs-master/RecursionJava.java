e5d6f2e683249e85eec686f6491bf30610574762
SkilledGit <73136887+SkilledGit@users.noreply.github.com>
2020-10-24 15:57:29 +0530

    Create RecursionJava.java

---
+++ Java_Programs-master/RecursionJava.java
@@ -0,0 +1,13 @@
+public class RecursionJava {
+  public static void main(String[] args) {
+    int result = sum(10);
+    System.out.println(result);
+  }
+  public static int sum(int k) {
+    if (k > 0) {
+      return k + sum(k - 1);
+    } else {
+      return 0;
+    }
+  }
+}
