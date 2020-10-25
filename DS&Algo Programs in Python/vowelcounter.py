16fc66122c7565aa223fe05dbd2f3e953113128c
Abhirami S A <abhiramisa97@gmail.com>
2020-10-25 14:46:32 +0530

    Update vowelcounter.py
    Made the code short and displayed the count

--- DS&Algo Programs in Python/vowelcounter.py
+++ DS&Algo Programs in Python/vowelcounter.py
@@ -1,19 +1,9 @@
 def countVowels(S):
     total_size = len(S)
-    vowels = 0
-
-    for char in S:
-        if char in 'aeiouAEIOU':
-            vowels = vowels + 1
+    vowelString = "AaEeIiOoUu"
+    vowels = len([each for each in S if each in vowelString])
     consonants = total_size - vowels
-    
-    if vowels > consonants:
-        print('There are more vowels than consonants in the string ' + S)
-    elif vowels == consonants:
-        print('Both vowels and consonants are equal in the string ' + S)
-    else:
-        print('There are more consonants than vowels in the string ' + S)
-
+    print("There are {} vowels and {} consonants in {}".format(vowels, consonants, S))
 countVowels('hello')
 countVowels('hi')
 countVowels('aeiocc')
