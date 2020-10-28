a4eadb820291bacdc918e556855e554b87edbdb7
Anurag Sharma <53372213+Anurag-0-1-A@users.noreply.github.com>
2020-10-28 13:46:14 +0530

    Update fib.py
    - Removed space
    - Added comments

--- fib.py
+++ fib.py
@@ -1,16 +1,17 @@
 term = int(input("Enter Terms: "))
+
+# variable initialization
 first = 0
 second = 1
 count = 0
+
+#condition for integer input
 if term <= 0:
     print("Positive Integers Only.")
 else:
     while count < term:
         print(first)
-        
         fib = first + second
-        
         first = second
         second = fib
-        
         count += 1
