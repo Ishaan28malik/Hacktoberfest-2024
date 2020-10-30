54b58ed3c9272b754a6080d250030a1b65dbc7cd
Tanishq More <44018380+tanishq-1011@users.noreply.github.com>
2020-10-30 23:29:36 +0530

    update hello.py

--- DS&Algo Programs in Python/hello.py
+++ DS&Algo Programs in Python/hello.py
@@ -3,10 +3,8 @@
 print('Hello World!')
 
 #fazendo um print com formatação, variáveis e função.
-
-def main():
-  mensagem = input('Digite a mensagem de apresentação : ')
+mensagem = input('Digite a mensagem de apresentação : ')
   
-  print(f' A mensagem digitada foi : {mensagem} !')
-main()  
+print(f' A mensagem digitada foi : {mensagem} !')
+  
 
