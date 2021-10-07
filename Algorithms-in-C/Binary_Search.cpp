f259ff4138e2cc8adde890dac9452cfb3600e095
prince0786 <57414351+prince0786@users.noreply.github.com>
2021-10-07 13:36:45 +0530

    Binary_Search.cpp

---
+++ Algorithms-in-C/Binary_Search.cpp
@@ -0,0 +1,28 @@
+#include
+using namespace std;
+int binarySearch(int arr[], int p, int r, int num) {
+   if (p <= r) {
+      int mid = (p + r)/2;
+      if (arr[mid] == num)
+         return mid ;
+      if (arr[mid] > num)
+         return binarySearch(arr, p, mid-1, num);
+      if (arr[mid] < num)
+         return binarySearch(arr, mid+1, r, num);
+   }
+   return -1;
+}
+int main(void) {
+   int arr[] = {1, 3, 7, 15, 18, 20, 25, 33, 36, 40};
+   int n = sizeof(arr)/ sizeof(arr[0]);
+   int num;
+   cout << "Enter the number to search: \n";
+   cin >> num;
+   int index = binarySearch (arr, 0, n-1, num);
+   if(index == -1){
+      cout<< num <<" is not present in the array";
+   }else{
+      cout<< num <<" is present at index "<< index <<" in the array";
+   }
+   return 0;
+}
