fa343b72081fde58134bfacc4d3078468ddede07
Nikita Jain <nikitajain81200@gmail.com>
2021-10-09 13:48:39 +0530

    quicksort
    hello there, I am willing to making a contribution by giving quick sort code in python.
    I hope this would be helpful, thankyou :)

---
+++ DS&Algo Programs in Python/quicksort.py
@@ -0,0 +1,17 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+
+    low, same, high = [], [], []
+
+    pivot = array[randint(0, len(array) - 1)]
+
+    for item in array:
+        if item < pivot:
+            low.append(item)
+        elif item == pivot:
+            same.append(item)
+        elif item > pivot:
+            high.append(item)
+
+    return quicksort(low) + same + quicksort(high)
