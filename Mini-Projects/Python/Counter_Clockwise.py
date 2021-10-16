b266a8e9a26a0dd4e89bd25f5d0c701b44f33773
ADVAIT TRIVEDI <63858681+advaittrivedi1122@users.noreply.github.com>
2021-10-16 22:03:21 +0530

    Add files via upload

---
+++ Mini-Projects/Python/Counter_Clockwise.py
@@ -0,0 +1,30 @@
+"""
+MATRIX ROTATION IN PLACE IN 
+COUNTER-CLOCKWISE DIRECTION
+"""
+a=[[1,2,3],[4,5,6],[7,8,9]]
+print('',a[0],'\n',a[1],'\n',a[2],'\n')
+
+# TRANSPOSE OF MATRIX
+for i in range(3):
+  for j in range(3):
+    if i<j:
+      a[i][j],a[j][i]=a[j][i],a[i][j]
+# print('',a[0],'\n',a[1],'\n',a[2],'\n')
+
+# SWAPPING 1st AND 3rd ROWS
+for i in range(3):
+  for j in range(1):
+    a[j][i],a[2-j][i]=a[2-j][i],a[j][i]
+print('',a[0],'\n',a[1],'\n',a[2])
+
+"""
+★MATRIX:-
+ [1, 2, 3]
+ [4, 5, 6]
+ [7, 8, 9]
+★After rotation:-
+ [3, 6, 9]
+ [2, 5, 8]
+ [1, 4, 7]
+"""
