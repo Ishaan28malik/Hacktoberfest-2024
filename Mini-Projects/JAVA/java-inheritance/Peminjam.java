4a2defd9e01c612978647a42485c699c91c6b63e
anwrtaufik <anwrtaufik@gmail.com>
2021-10-20 15:25:34 +0700

    add java inheritance project

---
+++ Mini-Projects/JAVA/java-inheritance/Peminjam.java
@@ -0,0 +1,15 @@
+package pertemuan_1.tugas.peminjaman;
+
+public class Peminjam {
+  public String nama;
+  public String alamat;
+  public String email;
+  public String noTelp;
+
+  public void setPeminjam(String nama, String alamat, String email, String noTelp) {
+    this.nama = nama;
+    this.alamat = alamat;
+    this.email = email;
+    this.noTelp = noTelp;
+  }
+}
