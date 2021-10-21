2b2c6dff53bf6d052ddf153930ca9f4787d56123
DivyanshuLohani <59534359+DivyanshuLohani@users.noreply.github.com>
2021-10-21 09:29:25 +0530

    added star pattern and binary to decimal conversion program

---
+++ Mini-Projects/Python/binary_to_decmal.py
@@ -0,0 +1,19 @@
+# Divyanshu Lohani: https://github.com/DivyanshuLohani
+
+# Function to print binary number for the
+# input decimal using recursion
+def decimalToBinary(n):
+ 
+    if(n > 1):
+        # divide with integral result
+        # (discard remainder)
+        decimalToBinary(n//2)
+ 
+     
+    print(n%2, end=' ')
+     
+     
+ 
+# Driver code
+if __name__ == '__main__':
+    decimalToBinary(8)
