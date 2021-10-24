db49834d5bcaede8e7bd89ab7771f3846f3d17b7
rahmah18 <56541229+rahmah18@users.noreply.github.com>
2021-10-24 22:54:46 +0100

    Update fib.py
    Creating fibonacci series

--- fib.py
+++ fib.py
@@ -14,3 +14,14 @@ else:
         second = fib
         
         count += 1
+
+ def fib():
+     n = int(input())
+     a = 0
+     b = 1
+     p = []
+     for i in range(n):
+         p.append(a)
+         a,b = b,a+b
+     print(p)
+fib()       
