41eedf6d12566b5b790a02675a4be7a1bf540e28
Prajakta Tipugade <122222331+prajaktatipugade@users.noreply.github.com>
2023-10-14 19:08:36 +0530

    Create stack.py

---
+++ DS&Algo Programs in Python/stack.py
@@ -0,0 +1,22 @@
+# initial empty stack  
+my_stack = []  
+  
+# append() function to push   
+# element in the my_stack   
+my_stack.append('x')  
+my_stack.append('y')  
+my_stack.append('z')  
+  
+  
+print(my_stack)  
+  
+# pop() function to pop   
+# element from my_stack in    
+# LIFO order   
+print('\nElements poped from my_stack:')  
+print(my_stack.pop())  
+print(my_stack.pop())  
+print(my_stack.pop())  
+  
+print('\nmy_stack after elements are poped:')  
+print(my_stack) 
