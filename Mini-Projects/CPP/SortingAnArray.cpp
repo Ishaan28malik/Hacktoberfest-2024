f93f085ac4c554a2324dc3ff64ecb1c2816065d7
skmd tousif <139031886+skmd-tousif@users.noreply.github.com>
2023-10-16 08:44:04 +0530

    Update SortingAnArray.cpp
    using bubble sort rather than sort function

--- Mini-Projects/CPP/SortingAnArray.cpp
+++ Mini-Projects/CPP/SortingAnArray.cpp
@@ -3,8 +3,16 @@ using namespace std;
 int main(){
   int n ;
   cin >> n ;
-  int* ar =new int[n];
-  for(int i =0 ;i < n ;i++)cin >> ar[i];
-  sort(ar,ar+n);
-  for(int i =0; i<n;i++)cout << ar[i] << " ";
+  int ar[n];
+  int temp;
+  for(int i =0 ;i < n ;i++){cin >> ar[i];}
+  for(int i=0;i<n;i++){
+      for(int j=i+1;j<n;j++){
+      if(ar[j]<ar[i]){
+      temp=ar[i];
+      ar[i]=ar[j];
+      ar[j]=temp;}
+      }
+  }
+  for(int i =0; i<n;i++){cout << ar[i] << " ";}
 }
