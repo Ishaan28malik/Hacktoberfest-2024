290aaaefb330a56c90716c40d9ae3a5499e5516e
Abdul Wahab <124174351+Wahab901278@users.noreply.github.com>
2023-10-23 19:18:56 +0500

    Updated CreateDataFrame.py by making more readable changes
    "Added documentation and comments for improved code readability."

--- Mini-Projects/Python/CreateDataFrame.py
+++ Mini-Projects/Python/CreateDataFrame.py
@@ -1,4 +1,11 @@
+# Importing Pandas Library
 import pandas as pd
+"""
+Making a dictionary of data with key-value pairs
+, Keys are Column names written outside Square Brackets
+and Values are the data written in lists
+"""
 dict={'Name':['Silvi','Shreya','Shaina','Shan'],'Roll No':[72,70,45,29],'Eng_Marks':[96,67,89,74],'Maths_Marks':[99,88,77,67]}
+# Data Frame function/method in pandas is used to create a dataframe of given data(dictionary) 
 df=pd.DataFrame(dict)
 print(df)
