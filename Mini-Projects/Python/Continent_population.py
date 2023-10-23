14c45354972a4ccab30c866d7a15921d2d3c25b7
Abdul Wahab <124174351+Wahab901278@users.noreply.github.com>
2023-10-23 19:09:09 +0500

    Updated Continent_population.py by making some useful changes
    "Improved Population in 7 Continents Finder code."
    "Added comments for better code readability."
    "Optimized the population data structure."
    "Enhanced user experience with error handling."

--- Mini-Projects/Python/Continent_population.py
+++ Mini-Projects/Python/Continent_population.py
@@ -1,24 +1,30 @@
+# Population in 7 Continents Finder
 
-print("Population in 7 Continents Finder")
+# Display the list of continents
 print("1. Asia")
 print("2. North America")
 print("3. South America")
 print("4. Africa")
 print("5. Europe")
-print("6. Antartica")
+print("6. Antarctica")
 print("7. Oceania")
-continent=int(input("which continent's population do you want to see? Type the Number."))
-if continent == 1:
-    print("the population of Asia is 4,762,417,975")
-if continent == 2:
-    print("the population of North America is 379,530,629")
-if continent == 3:
-    print("the population of South America is 666,449,790")
-if continent == 4:
-    print("the population of Africa is 1,470,500,688")
-if continent == 5:
-    print("the population of Europe is 742,090,883")
-if continent == 6:
-    print("the population of Antartica is 5000")
-if continent == 7:
-    print("the population of Oceania is 45,731,277")
+
+# Prompt the user to choose a continent
+continent = int(input("Which continent's population do you want to see? Enter the number: "))
+
+# Define population data for each continent
+population_data = {
+    1: "4,762,417,975",
+    2: "379,530,629",
+    3: "666,449,790",
+    4: "1,470,500,688",
+    5: "742,090,883",
+    6: "5,000",
+    7: "45,731,277"
+}
+
+# Check the user's choice and display the population
+if continent in population_data:
+    print(f"The population of the selected continent is {population_data[continent]}.")
+else:
+    print("Invalid selection. Please choose a valid continent.")
