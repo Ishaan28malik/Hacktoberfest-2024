447704955bd1241a48cef6440fbf8db9991b8b03
Rachit Rai <130988685+Rai1706@users.noreply.github.com>
2024-09-30 09:33:32 +0530

    Update Even_Odd.java

--- Java_Programs-master/Even_Odd.java
+++ Java_Programs-master/Even_Odd.java
@@ -5,14 +5,14 @@ class CheckEvenOdd
   public static void main(String args[])
   {
     int num;
-    System.out.println("Enter an Integer number:");
+    System.out.println("Enter an integer:");
 
-    //The input provided by user is stored in num
+    //The input provided by the user is stored in num
     Scanner input = new Scanner(System.in);
     num = input.nextInt();
 
-    /* If number is divisible by 2 then it's an even number
-     * else odd number*/
+    /* If the number is divisible by 2 then it's an even number
+     * else the number is odd*/
     if ( num % 2 == 0 )
         System.out.println("Entered number is even");
      else
